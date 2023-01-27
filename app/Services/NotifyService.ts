a832e54ba58283a1033c842140a13728718cb078
Assis Ferreira <ferreira.assisruny@gmail.com>
2023-01-27 22:58:34 -0100

    feat: post message to kafka

--- app/Services/NotifyService.ts
+++ app/Services/NotifyService.ts
@@ -1,8 +1,33 @@
+import { Kafka } from 'kafkajs'
+
 export default class NotifyService {
 
     public static async sendToQueue(event)
     {
         console.log("Send to Queue: " + JSON.stringify(event));
+
+        try
+        {
+            const kafka = new Kafka({
+                clientId: 'my-app',
+                brokers: ['kafka:9093'],
+            })
+
+            const producer = kafka.producer()
+
+            await producer.connect()
+            await producer.send({
+                topic: 'task-topic',
+                messages: [
+                    { value: JSON.stringify(event) },
+                ],
+            })
+            await producer.disconnect()
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
     }
 
 }
